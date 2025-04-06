1ecb241246428e45d21aa436054558824152acd2
RayiAqliGemilangIBU <rayi.aqli.gemilang@stu.ibu.edu.ba>
2025-04-06 12:34:49 +0200

    update Basedao

--- backend/rest/dao/BaseDao.php
+++ backend/rest/dao/BaseDao.php
@@ -9,10 +9,17 @@ class BaseDao {
     }
 
     public function insert($table, $data) {
+        // Menentukan kolom dan nilai yang akan dimasukkan
         $columns = implode(", ", array_keys($data));
-        $placeholders = implode(", ", array_map(fn($col) => ":$col", array_keys($data)));
+        $placeholders = ":" . implode(", :", array_keys($data));
+    
+        // Menyiapkan query insert
         $stmt = $this->conn->prepare("INSERT INTO $table ($columns) VALUES ($placeholders)");
+        
+        // Menjalankan query dengan data
         $stmt->execute($data);
+        
+        // Mengembalikan ID terakhir yang dimasukkan
         return $this->conn->lastInsertId();
     }
 
@@ -28,11 +35,23 @@ class BaseDao {
     }
 
     public function update($table, $data, $idField, $id) {
+        // // Menyusun kolom dan nilai untuk query UPDATE
+        // $fields = implode(", ", array_map(fn($col) => "$col = :$col", array_keys($data)));
+    
+        // // Menyusun query UPDATE
+        // $stmt = $this->conn->prepare("UPDATE $table SET $fields WHERE $idField = :id");
+        
+        // // Menambahkan ID ke dalam data untuk binding
+        // $data['id'] = $id;
+        
+        // // Menjalankan query update
+        // return $stmt->execute($data);
         $fields = implode(", ", array_map(fn($col) => "$col = :$col", array_keys($data)));
         $stmt = $this->conn->prepare("UPDATE $table SET $fields WHERE $idField = :id");
-        $data['id'] = $id;
+        $data['id'] = $id; // Pastikan 'id' adalah nama kolom yang benar
         return $stmt->execute($data);
     }
+    
 
     public function delete($table, $idField, $id) {
         $stmt = $this->conn->prepare("DELETE FROM $table WHERE $idField = ?");
