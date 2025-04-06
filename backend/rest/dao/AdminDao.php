06c7e4e6ab8b8921b71872ee56e76de93791b698
RayiAqliGemilangIBU <rayi.aqli.gemilang@stu.ibu.edu.ba>
2025-04-06 09:09:02 +0200

    make admin dao

--- backend/rest/dao/AdminDao.php
+++ backend/rest/dao/AdminDao.php
@@ -2,7 +2,23 @@
 require_once 'BaseDao.php';
 
 class AdminDao extends BaseDao {
-    public function insert($user_id) {
-        return parent::insert('Admin', ['user_id' => $user_id]);
+
+    public function insert($table, $data) {
+        $columns = implode(", ", array_keys($data));
+        $values = ":" . implode(", :", array_keys($data));
+
+        $sql = "INSERT INTO $table ($columns) VALUES ($values)";
+
+        $stmt = $this->conn->prepare($sql);
+
+        foreach ($data as $key => $value) {
+            $stmt->bindValue(":$key", $value);
+        }
+
+        if ($stmt->execute()) {
+            return $this->conn->lastInsertId(); 
+        } else {
+            return false;
+        }
     }
 }
